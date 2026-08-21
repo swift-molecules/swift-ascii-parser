5b4309d9f7c3b9e75b4c8085525bf1f4bf8ffd79
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 07:07:50 +0200

    public import warning fix

--- Tests/Support/Byte.Input+Bytes.swift
+++ Tests/Support/Byte.Input+Bytes.swift
@@ -30,10 +30,10 @@ public import Buffer_Linear_Primitive
 import Buffer_Linear_Primitives
 public import Byte_Parser_Primitives
 import Byte_Primitives
-public import Column_Primitives
+import Column_Primitives
 import Input_Primitives
 public import Memory_Heap_Primitives
-public import Ownership_Shared_Primitive
+import Ownership_Shared_Primitive
 public import Storage_Contiguous_Primitives
 
 extension Input.Slice where Base == Array<Byte>.Shared {
